e84d934f475d7b034b936fe46e9c2cd6f5b24689
AryanSharma2009 <78365089+AryanSharma2009@users.noreply.github.com>
2021-12-30 19:04:27 +0530

    Add files via upload

---
+++ page3.js
@@ -0,0 +1,42 @@
+img = "";
+status = "";
+
+function preload(){
+    img = loadImage('dog_cat.jpg');
+
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+     
+    fill("#FF0000");
+    text("Cat", 310, 115);
+    noFill();
+    stroke("#FF0000");
+    rect(290, 100, 320, 300);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+}
+
+
+function back() {
+    window.location = "index.html";
+}
